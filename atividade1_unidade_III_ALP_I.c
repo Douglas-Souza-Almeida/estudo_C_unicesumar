7765853fa3ea8bb31c0c6dea660a71880ac6f2eb
Douglas de Souza Almeida <61165006+Douglas-Souza-Almeida@users.noreply.github.com>
2020-05-20 19:58:13 -0300

    Envio de atividade de estudo.
    Código referente a atividade 1, da unidade 3, do livro Algoritmo e Lógica de Programação 1, do curso de Engenharia de Software, da UniCesumar:
    "Escreva um algoritmo que leia a quantidade de números que se deseja digitar. Em seguida, leia esses números e encontre o maior e o menor."

---
+++ atividade1_unidade_III_ALP_I.c
@@ -0,0 +1,32 @@
+﻿/*Escreva um algoritmo que leia a quantidade de números que se deseja digitar. Em
+seguida, leia esses números e encontre o maior e o menor.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int maior, menor, qtd, cont, num;
+    
+    printf("Informe a quantidade de números que será analisado: ");
+    scanf("%d",&qtd);
+    cont = 1;
+    printf("Informe um número: ");
+    scanf("%d",&num);
+    maior = num;
+    menor = num;
+    while(cont < qtd){
+         printf("Informe outro número: ");
+         scanf("%d",&num);
+        if (num > maior){
+            maior = num;
+        }else{
+        if (num < menor){
+            menor = num;
+        }
+        }
+    cont = cont + 1;
+    }
+    printf("\nO maior valor digitado foi: %d", maior);
+    printf("\nO menor valor digitado foi: %d", menor);
+    return 0;
+}
