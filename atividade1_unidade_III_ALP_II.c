957ac9782083e77dbbb6ed84c90518affd475c0c
Douglas de Souza Almeida <61165006+Douglas-Souza-Almeida@users.noreply.github.com>
2020-05-22 20:40:43 -0300

    Envio de atividade

---
+++ atividade1_unidade_III_ALP_II.c
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+#include <locale.h>
+int main()
+{
+  setlocale(LC_ALL,"");
+  int num, tell, even, odd, bigger, smaller;
+  float sum;
+  printf("Informe um número: ");
+  scanf("%d",&num);
+  tell = 0;
+  sum = 0;
+  even = 0;
+  odd = 0;
+  smaller = num;
+  bigger = num;
+  while(num != 0){
+      tell++;
+      sum = sum + num;
+      if(num % 2 == 0){
+          even++;
+      }else{
+          odd++;
+      }
+      if(num > bigger){
+          bigger = num;
+      }else{
+        if(num < smaller){
+          smaller = num;
+        }
+      }
+        printf("Informe outro número: ");
+        scanf("%d",&num);
+  }
+printf("\nA soma dos números digitados é: %.2f", sum);
+printf("\nO total de números pares digitados foi de: %d", even);
+printf("\nO total de números ímpares digitados foi de: %d", odd);
+printf("\nA média dos números digitado é de: %.2f", sum/tell);
+printf("\nO total de números válidos digitados foi: %d", tell);
+printf("\nO maior número digitado foi: %d", bigger);
+printf("\nO menor número digitado foi: %d", smaller);
+
+return 0;
+}
